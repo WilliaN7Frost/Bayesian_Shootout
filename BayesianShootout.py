5133f03a6f28ec7b3ad1c82f94b0f7adb3f6c49d
{William <{william.frost2@mail.mcgill.ca}>
2020-04-09 00:08:35 -0400

    added the one-liner code that lets you play the game

---
+++ BayesianShootout.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  8 23:05:47 2020
+
+@author: wilia
+"""
+
+import BayesianShootoutFuncs_2 as b
+
+# Dont forget you can customize pretty much all parameters of the game
+# Not everything needs to be randomized!
+b.beginGame(bSeper=0. , your_loc=[750.,250.,500.])
