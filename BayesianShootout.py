70dea7986d3ff8da485aff248577cf567b2f8136
{William <{william.frost2@mail.mcgill.ca}>
2020-04-09 02:42:12 -0400

    made it simpler

--- BayesianShootout.py
+++ BayesianShootout.py
@@ -9,4 +9,4 @@ import BayesianShootoutFuncs_2 as b
 
 # Dont forget you can customize pretty much all parameters of the game
 # Not everything needs to be randomized!
-b.beginGame(bSeper=0. , your_loc=[750.,250.,500.])
+b.beginGame()
