b94fd8d5712207213e5fdc1091afc78c7d55346d
{William <{william.frost2@mail.mcgill.ca}>
2020-04-14 19:37:32 -0400

    added beginGame() header as comment

--- BayesianShootout.py
+++ BayesianShootout.py
@@ -5,8 +5,13 @@ Created on Wed Apr  8 23:05:47 2020
 @author: wilia
 """
 
-import BayesianShootoutFuncs_2 as b
+import BayesianShootoutFuncs_3 as b
 
 # Dont forget you can customize pretty much all parameters of the game
 # Not everything needs to be randomized!
+# Function header:
+#       beginGame( blue_loc=[] , red_loc=[] , blue_hitbox=[] , red_hitbox = [] , buildingDims=[] , bSeper=0. , 
+#       hit_tolerance=0 , confidence_tolerance=0.68 , emceeAtEach=10 , start_MCing_at = 20 , shotsAllowed=60 , 
+#       log_post1=True , easyMode=False , hardMode=True , doPrint=True , doPlot=True , doCornerPlot=True ):
+
 b.beginGame()
